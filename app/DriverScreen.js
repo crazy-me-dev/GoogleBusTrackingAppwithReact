dc0e7e4f521a6962eb35ae232f7193cb0a76c33a
chirsworking <chrisjorgework@protonmail.com>
2018-02-11 11:15:57 -0500

    potentially working on ios?

--- app/DriverScreen.js
+++ app/DriverScreen.js
@@ -15,19 +15,41 @@ export default class DriverScreen extends React.Component {
       latitude: 'initial',
       longitude: 'initial',
     };
+    this.setUpWebSocket();
+  }
+
+  setUpWebSocket() {
+    this.websocket = new WebSocket('ws://192.168.0.178:3000');
+    this.websocket.onopen = () => {
+      // connection opened
+      console.log('connected to server');
+      this.websocket.send(JSON.stringify({
+        method: 'publish',
+        locationID: '123',
+      }));
+    };
+    // this.websocket.onmessage = (message) => {
+    //   console.log('received message from server', JSON.parse(message.data));
+    // };
   }
 
   componentDidMount() {
     navigator.geolocation.getCurrentPosition(
       (position) => {
+        console.log(position);
         this.setState({
           latitude: position.coords.latitude,
           longitude: position.coords.longitude,
           error: null,
         });
+        this.websocket.send(JSON.stringify({
+          method: 'emitLocation',
+          location: position.coords,
+          locationID: '123'
+        }));
       },
       (error) => this.setState({ error: error.message }),
-      { enableHighAccuracy: true, timeout: 5000, maximumAge: 1000 },
+      { enableHighAccuracy: true, timeout: 10000, maximumAge: 1000 },
     );
   }
 
--- app/ParentScreen.js
+++ app/ParentScreen.js
@@ -6,7 +6,7 @@ import {
   Text,
   View
 } from 'react-native';
-import MapView, { Polyline, Marker } from 'react-native-maps';
+import MapView, { Polyline, Marker, PROVIDER_GOOGLE } from 'react-native-maps';
 
 export default class ParentScreen extends React.Component {
 
@@ -14,7 +14,7 @@ export default class ParentScreen extends React.Component {
     super(props);
 
     const dataSource = new ListView.DataSource({ rowHasChanged: this.rowHasChanged });
-    const testLocationArray = [{ latitude: 39.7684, longitude: -86.1581 }];
+    const startLocation = { latitude: 39.7684, longitude: -86.1581 };
     this.state = {
       region: {
         latitude: 39.7684,
@@ -23,30 +23,40 @@ export default class ParentScreen extends React.Component {
         longitudeDelta: .5,
       },
       dataSource,
-      locations: testLocationArray,
-      current: testLocationArray[testLocationArray.length - 1],
-      locationRows: dataSource.cloneWithRows(testLocationArray)
+      locations: [startLocation],
+      current: startLocation,
+      locationRows: dataSource.cloneWithRows([startLocation])
     };
-    let ws = new WebSocket('ws://192.168.0.178:3000');
-    ws.onopen = () => {
+    this.setUpWebSocket();
+  }
+
+  setUpWebSocket() {
+    this.websocket = new WebSocket('ws://192.168.0.178:3000');
+    this.websocket.onopen = () => {
       // connection opened
+      this.websocket.send(JSON.stringify({
+        method: 'subscribe',
+        locationID: '123',
+      }));
       console.log('connected my person');
     };
-    ws.onmessage = (message) => {
-      const location = JSON.parse(message.data);
-      const newLocations = this.state.locations.slice(0);
-      newLocations.push(location);
-      this.setState({
-        region: {
-          latitude: location.latitude,
-          longitude: location.longitude,
-          latitudeDelta: .5,
-          longitudeDelta: .5,
-        },
-        locations: newLocations,
-        current: location,
-        locationRows: this.state.dataSource.cloneWithRows(newLocations)
-      });
+    this.websocket.onmessage = (message) => {
+      let data = JSON.parse(message.data);
+      if (data.method === 'LatestLocation') {
+        const newLocations = this.state.locations.slice(0);
+        newLocations.push({ latitude: data.latitude, longitude: data.longitude });
+        this.setState({
+          region: {
+            latitude: data.latitude,
+            longitude: data.longitude,
+            latitudeDelta: .5,
+            longitudeDelta: .5,
+          },
+          locations: newLocations,
+          current: { latitude: data.latitude, longitude: data.longitude },
+          locationRows: this.state.dataSource.cloneWithRows(newLocations)
+        });
+      }
     };
   }
 
@@ -66,6 +76,7 @@ export default class ParentScreen extends React.Component {
     return (
       <View style={styles.container}>
         <MapView
+          provider={PROVIDER_GOOGLE}
           style={styles.map}
           region={this.state.region}
           onRegionChange={this.onRegionChange.bind(this)}>
